081a45a649a83517220cca46496dc83ab9c0957f
Shirley Ascencio <shirley.ascencio@vallegrande.edu.pe>
2024-04-11 10:49:56 -0500

    [feature1]: sp2-proyecto base

---
+++ src/main/java/com/service/cognitive/speech/SpeechApplication.java
@@ -0,0 +1,13 @@
+package com.service.cognitive.speech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeechApplication.class, args);
+	}
+
+}
