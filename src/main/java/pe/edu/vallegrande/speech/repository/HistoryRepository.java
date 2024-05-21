fd960e9f2efe825dbb4ca5f44357d30dc707baba
16-ValeryChumpitaz <you@example.com>
2024-05-20 19:25:10 -0500

    delete

--- src/main/java/pe/edu/vallegrande/speech/repository/HistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.vallegrande.speech.repository;
-
-import org.springframework.data.r2dbc.repository.R2dbcRepository;
-import pe.edu.vallegrande.speech.model.History;
-import reactor.core.publisher.Flux;
-
-public interface HistoryRepository extends R2dbcRepository<History, Long> {
-    Flux<History> findAll();
-}
